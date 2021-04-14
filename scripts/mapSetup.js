0f6afd7c0e64501360f98e0c7890e07e436ee690
katecourt <nkc124@ncl.ac.uk>
2021-04-14 11:52:14 +0100

    test papa parse data

--- scripts/mapSetup.js
+++ scripts/mapSetup.js
@@ -13,12 +13,18 @@ $(window).on('load', function() {
     });
   }
 
+    var parse = function(res) {
+        return Papa.parse(Papa.unparse(res[0].values), {header: true} ).data;
+    }
+
   $.getJSON(
-      "https://sheets.googleapis.com/v4/spreadsheets/10hoBLjIu0_qothMvChC8mkiI_QyEh7KCwOs4TFxq0kk?key=AIzaSyBx3U9RNDkd76gmJ8YfFR3CHlTNP5eF9kk",
+      "https://sheets.googleapis.com/v4/spreadsheets/10hoBLjIu0_qothMvChC8mkiI_QyEh7KCwOs4TFxq0kk/values/Sheet1?key=AIzaSyBx3U9RNDkd76gmJ8YfFR3CHlTNP5eF9kk",
       (data) => {
-          console.log(data)
+         var parsedData = parse(data)
+          alert(parsedData[0])
       }
       )
+
 
   
   
