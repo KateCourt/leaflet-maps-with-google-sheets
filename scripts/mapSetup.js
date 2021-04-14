8cadfe121ae129f1fc360cbd27682453c2acf824
katecourt <nkc124@ncl.ac.uk>
2021-04-14 15:32:46 +0100

    add markers for each sheets entry

--- scripts/mapSetup.js
+++ scripts/mapSetup.js
@@ -17,13 +17,14 @@ $(window).on('load', function() {
   $.getJSON(
       "https://sheets.googleapis.com/v4/spreadsheets/10hoBLjIu0_qothMvChC8mkiI_QyEh7KCwOs4TFxq0kk/values/Sheet1?key=AIzaSyBx3U9RNDkd76gmJ8YfFR3CHlTNP5eF9kk",
       (data) => {
-        console.log('test3')
+        console.log('test4')
 
           var datajson = Papa.parse(Papa.unparse(data['values']), {header: true} ).data
           // todo check for errors
           var i;
         for (i = 0; i < datajson.length; i++) {
-            L.marker([datajson[i].latitude, datajson[i].longitude]).addTo(map)
+            console.log(datajson[i])
+            // L.marker([datajson[i].latitude, datajson[i].longitude]).addTo(map)
         }
       }
       )
