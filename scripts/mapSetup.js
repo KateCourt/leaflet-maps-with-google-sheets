269b6aeac56dbc101658b90f89f8f7762e89da78
katecourt <nkc124@ncl.ac.uk>
2021-04-21 11:14:22 +0100

    add table

--- scripts/mapSetup.js
+++ scripts/mapSetup.js
@@ -11,7 +11,7 @@ $(window).on('load', function() {
           var parsedData = Papa.parse(Papa.unparse(data['values']), {header: true} ).data
           // todo check for errors
 
-
+            createTable(parsedData)
 
           // get unique list of all locations
           var countries = []
@@ -39,6 +39,49 @@ $(window).on('load', function() {
       )
 
     //--------------------- Utility methods
+
+    // create table
+    function createTable(jsonData) {
+        // EXTRACT VALUE FOR HTML HEADER.
+        var col = [];
+        for (var i = 0; i < jsonData.length; i++) {
+            for (var key in jsonData[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < jsonData.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = jsonData[i][col[j]];
+            }
+        }
+
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        var divContainer = document.getElementById("showData");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+    }
+
 
   // Returns an Awesome marker with specified parameters
     function createMarkerIcon(icon, prefix, markerColor, iconColor) {
